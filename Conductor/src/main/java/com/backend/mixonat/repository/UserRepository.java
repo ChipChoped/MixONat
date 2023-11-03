a2f7ceed2b1c58515ff7463beaeec7b1f14c9604
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2023-11-03 22:38:35 +0100

    Find user by email query

--- Conductor/src/main/java/com/backend/mixonat/repository/UserRepository.java
+++ Conductor/src/main/java/com/backend/mixonat/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import com.backend.mixonat.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +15,9 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query(value="SELECT id FROM users WHERE email = ?1 AND password = ?2", nativeQuery = true)
     List<Integer> findUserIDByLogins(String email, String password);
 
-    @Query(value="SELECT id, firstname, lastname, email, status FROM users WHERE id = ?1", nativeQuery = true)
+    @Query(value="SELECT id, first_name, last_name, email, role FROM users WHERE id = ?1", nativeQuery = true)
     List<User> findUserByID(int id);
+
+//    @Query(value="SELECT email, password, role FROM users WHERE email = ?1", nativeQuery = true)
+    Optional<User> findUserByEmail(String email);
 }
