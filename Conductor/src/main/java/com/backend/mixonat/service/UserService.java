2623d57502ae994cfe72fb917a704d43c8c0fa24
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2023-11-03 22:44:43 +0100

    Can save new user

--- Conductor/src/main/java/com/backend/mixonat/service/UserService.java
+++ Conductor/src/main/java/com/backend/mixonat/service/UserService.java
@@ -1,26 +1,50 @@
 package com.backend.mixonat.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-import com.backend.mixonat.dto.Logins;
+import com.backend.mixonat.dto.LoginsDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.backend.mixonat.model.User;
 import com.backend.mixonat.repository.UserRepository;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
     @Autowired
-    UserRepository UserRepository;
+    private final UserRepository userRepository;
 
-    public List<Integer> findUserIDByLogins(Logins logins)
-    {
-        return UserRepository.findUserIDByLogins(logins.getEmail(), logins.getPassword());
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) {
+                return userRepository.findUserByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+
+    public User save(User user) {
+        if (user.getId() == null)
+            user.setCreatedAt(LocalDateTime.now());
+
+        user.setUpdatedAt(LocalDateTime.now());
+
+        return userRepository.save(user);
+    }
+
+    public List<Integer> findUserIDByLogins(LoginsDTO loginsDTO) {
+        return userRepository.findUserIDByLogins(loginsDTO.getEmail(), loginsDTO.getPassword());
     }
 
     public List<User> findUserByID(int id)
     {
-        return UserRepository.findUserByID(id);
+        return userRepository.findUserByID(id);
     }
 }
