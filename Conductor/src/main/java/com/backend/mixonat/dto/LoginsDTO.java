40b27e08ef0f6ce2f361fd2d13de2bd297118e02
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2023-11-05 12:31:39 +0100

    Logins validation

--- Conductor/src/main/java/com/backend/mixonat/dto/LoginsDTO.java
+++ Conductor/src/main/java/com/backend/mixonat/dto/LoginsDTO.java
@@ -1,5 +1,8 @@
 package com.backend.mixonat.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +13,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginsDTO {
+    @Email(message = "Email is malformed")
+    @NotBlank(message = "Email cannot be blank")
     String email;
+
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^*-]).{8,16}$",
+            message = "Invalid characters in password")
+    @NotBlank(message = "Password cannot be blank")
     String password;
 }
