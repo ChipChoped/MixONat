719534fe904ce88e3bfe03b01f545966706067ea
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2023-12-16 16:31:53 +0100

    SDF roots tests

---
+++ Conductor/src/test/java/com/backend/mixonat/SdfTest.java
@@ -0,0 +1,28 @@
+package com.backend.mixonat;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SdfTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/rmn/sdf/names")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Lamiaceae")));
+    }
+}
