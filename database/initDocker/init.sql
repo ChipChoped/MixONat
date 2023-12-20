7f9f676cc79966d0e2b0cf93206e7a309bbb973f
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2023-12-20 20:28:59 +0100

    Added rmn table and uuid to users

--- database/initDocker/init.sql
+++ database/initDocker/init.sql
@@ -12,6 +12,7 @@ $$;
 \connect mixonat
 
 DROP TABLE IF EXISTS sdf;
+DROP TABLE IF EXISTS rmn;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE sdf (
@@ -19,12 +20,25 @@ CREATE TABLE sdf (
 	name TEXT,
 	sdf_file TEXT);
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS rmn (
     id SERIAL PRIMARY KEY,
+    name TEXT,
+    rmn_file TEXT);
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    id SERIAL,
+    uuid UUID PRIMARY KEY NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    email VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
     password CHAR(60) NOT NULL,
     role VARCHAR(20),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW());
+
+INSERT INTO users (first_name, last_name, email, password, role)
+VALUES ('admin', 'admin', 'admin@mixonat.fr',
+        '$2a$06$hxJgB9phgXE1N0PzFJGcHeFrMjHzcYouIAgifa/AKlHemKgr9cr1q', -- 1pwdAdmin
+        'ROLE_ADMIN');
