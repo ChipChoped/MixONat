0540dcb3ea0b1812a12d1858101ed7425dac2df2
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2023-12-21 21:05:46 +0100

    Create UuidDTO.java

---
+++ Conductor/src/main/java/com/backend/mixonat/dto/UuidDTO.java
@@ -0,0 +1,16 @@
+package com.backend.mixonat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UuidDTO implements JsonResponse {
+    private UUID uuid;
+}
