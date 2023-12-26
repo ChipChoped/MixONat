0310db154685dc33edf2fc09040f591bad5e167a
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2023-12-26 17:43:18 +0100

    From FromtRequest to MotorDTO

---
+++ Conductor/src/main/java/com/backend/mixonat/model/MotorDTO.java
@@ -0,0 +1,18 @@
+package com.backend.mixonat.model;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class MotorDTO implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+    String sdf;
+    String spectrum;
+    String dept135;
+    String dept90;
+    Parametres params;
+    Boolean useSdfDatabase;
+    Boolean useRmnDatabase;
+}
