9e6b85e192a7d6f3fb1ed893acf1fff80d2c35c9
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2023-12-26 17:41:29 +0100

    Custom User save function

--- Conductor/src/main/java/com/backend/mixonat/repository/UserRepository.java
+++ Conductor/src/main/java/com/backend/mixonat/repository/UserRepository.java
@@ -1,27 +1,29 @@
 package com.backend.mixonat.repository;
 
+import com.backend.mixonat.model.Role;
+import com.backend.mixonat.model.User;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.backend.mixonat.model.User;
-
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-    @Query(value="SELECT id FROM users WHERE email = ?1 AND password = ?2", nativeQuery = true)
-    List<Integer> findUserIDByLogins(String email, String password);
-
-    @Query(value="SELECT id, first_name, last_name, email, role FROM users WHERE id = ?1", nativeQuery = true)
-    List<User> findUserByID(int id);
-
+public interface UserRepository extends JpaRepository<User, UUID> {
     @Query(value="SELECT uuid FROM users WHERE email = ?1", nativeQuery = true)
     UUID findUuidByEmail(String email);
 
     Optional<User> findUserByUuid(UUID uuid);
     Optional<User> findUserByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query(value = "insert into users (first_name, last_name, email, password, role)" +
+            "values (:#{#first_name}, :#{#last_name}, :#{#email}, :#{#password}, :#{#role});", nativeQuery = true)
+    User save(@Param("first_name") String first_name, @Param("last_name") String last_name, @Param("email") String email, @Param("password") String password, @Param("role") Role role);
 }
+
--- Conductor/src/main/java/com/backend/mixonat/service/UserService.java
+++ Conductor/src/main/java/com/backend/mixonat/service/UserService.java
@@ -1,19 +1,14 @@
 package com.backend.mixonat.service;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
-
-import com.backend.mixonat.dto.LoginsDTO;
+import com.backend.mixonat.model.User;
+import com.backend.mixonat.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.backend.mixonat.model.User;
-import com.backend.mixonat.repository.UserRepository;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -27,20 +22,6 @@ public class UserService {
     }
 
     public User save(User user) {
-        if (user.getUuid() == null)
-            user.setCreatedAt(LocalDateTime.now());
-
-        user.setUpdatedAt(LocalDateTime.now());
-
-        return userRepository.save(user);
-    }
-
-    public List<Integer> findUserIDByLogins(LoginsDTO loginsDTO) {
-        return userRepository.findUserIDByLogins(loginsDTO.getEmail(), loginsDTO.getPassword());
-    }
-
-    public List<User> findUserByID(int id)
-    {
-        return userRepository.findUserByID(id);
+        return userRepository.save(user.getFirstName(), user.getLastName(), user.getEmail(), user.getPassword(), user.getRole());
     }
 }
