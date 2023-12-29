143f77aec164ce67a65b0f966c6b92e140232d38
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2023-12-29 15:09:26 +0100

    DTOs to send files info and content

---
+++ Conductor/src/main/java/com/backend/mixonat/dto/RmnDTO.java
@@ -0,0 +1,23 @@
+package com.backend.mixonat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RmnDTO implements JsonResponse {
+    private UUID uuid;
+    private String name;
+    private String file;
+    private String author;
+    private UUID added_by;
+    private String added_by_name;
+    private LocalDateTime added_at;
+}
